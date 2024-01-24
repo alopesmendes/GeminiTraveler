b1545dba0db56b878d88ddefb86e7811f616e1aa
Justine Pataco <justinepataco@gmail.com>
2024-01-24 16:40:14 +0100

    :lipstick: [#2] Created components for speech bubble and chat row

---
+++ app/src/main/java/com/ippon/geminitraveler/ui/components/ChatRow.kt
@@ -0,0 +1,48 @@
+package com.ippon.geminitraveler.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ippon.geminitraveler.R
+
+@Composable
+fun ChatRow(speechContent: String, isGemini: Boolean) {
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    )
+    {
+        var backgroundColor = MaterialTheme.colorScheme.primary
+        if (isGemini) {
+            backgroundColor = MaterialTheme.colorScheme.tertiary
+            Image(
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(32.dp),
+                painter = painterResource(id = R.drawable.ic_robot),
+                contentDescription = "AI icon"
+            )
+        }
+        SpeechBubble(color = backgroundColor, content = speechContent, isGemini = isGemini)
+    }
+}
+
+@Composable
+@Preview(apiLevel = 33)
+fun ChatRowPreview() {
+    Column {
+        ChatRow(speechContent = "Hello, I'm Gemini", isGemini = true)
+        ChatRow(speechContent = "Plan me a vacation", isGemini = false)
+    }
+}
---
+++ app/src/main/java/com/ippon/geminitraveler/ui/components/SpeechBubble.kt
@@ -0,0 +1,48 @@
+package com.ippon.geminitraveler.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ippon.geminitraveler.core.components.MarkdownText
+import com.ippon.geminitraveler.ui.theme.md_theme_light_onTertiary
+import com.ippon.geminitraveler.ui.theme.md_theme_light_primary
+
+@Composable
+fun SpeechBubble(color: Color, content: String, isGemini: Boolean) {
+    Row {
+        val cardShape: RoundedCornerShape by remember(isGemini) {
+            derivedStateOf {
+                if (isGemini) RoundedCornerShape(8.dp, 8.dp, 8.dp, 0.dp)
+                else RoundedCornerShape(8.dp, 8.dp, 0.dp, 8.dp)
+            }
+        }
+        ElevatedCard(
+            shape = cardShape,
+            colors = CardDefaults.cardColors(
+                containerColor = color,
+                contentColor = md_theme_light_onTertiary
+            )
+        ) {
+            Column(modifier = Modifier.padding(12.dp)) {
+                MarkdownText(markdownContent = content)
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SpeechBubblePreview() {
+    SpeechBubble(color = md_theme_light_primary, content = "Hello I'm Gemini", isGemini = true)
+}
