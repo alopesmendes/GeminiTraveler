84b5f589eded35906cac7c0bd1cfb65c0f47f1e0
Justine Pataco <justinepataco@gmail.com>
2024-01-24 18:04:30 +0100

    :lipstick: [#2] Added custom text field and modified send button to icon button

---
+++ app/src/main/java/com/ippon/geminitraveler/ui/components/CustomTextField.kt
@@ -0,0 +1,37 @@
+package com.ippon.geminitraveler.ui.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ippon.geminitraveler.R
+import com.ippon.geminitraveler.ui.theme.md_theme_light_onTertiary
+import com.ippon.geminitraveler.ui.theme.md_theme_light_tertiary
+
+@Composable
+fun CustomTextField(prompt: String, onPromptChange: (String) -> Unit) {
+    TextField(
+        value = prompt,
+        placeholder = { Text(stringResource(R.string.summarize_hint)) },
+        onValueChange = { onPromptChange(it) },
+        shape = RoundedCornerShape(100),
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = md_theme_light_tertiary,
+            focusedContainerColor = md_theme_light_tertiary,
+            focusedTextColor = md_theme_light_onTertiary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+
+        )
+    )
+}
+
+@Composable
+@Preview
+fun CustomTextFieldPreview() {
+    CustomTextField(prompt = "", onPromptChange = { })
+}
--- app/src/main/java/com/ippon/geminitraveler/ui/screens/ChatbotScreen.kt
+++ app/src/main/java/com/ippon/geminitraveler/ui/screens/ChatbotScreen.kt
@@ -1,9 +1,14 @@
 package com.ippon.geminitraveler.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -11,8 +16,6 @@ import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,13 +24,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ippon.geminitraveler.R
 import com.ippon.geminitraveler.core.components.MarkdownText
 import com.ippon.geminitraveler.core.components.StateContainer
 import com.ippon.geminitraveler.core.utils.State
 import com.ippon.geminitraveler.domain.model.PlanTravel
+import com.ippon.geminitraveler.ui.components.CustomTextField
+import com.ippon.geminitraveler.ui.components.SendButton
 
 @Composable
 fun ChatbotScreen(
@@ -38,32 +42,24 @@ fun ChatbotScreen(
 
     Column(
         modifier = Modifier
+            .fillMaxSize()
             .padding(all = 8.dp)
             .verticalScroll(rememberScrollState())
     ) {
-        Row {
-            TextField(
-                value = prompt,
-                label = { Text(stringResource(R.string.summarize_label)) },
-                placeholder = { Text(stringResource(R.string.summarize_hint)) },
-                onValueChange = { prompt = it },
-                modifier = Modifier
-                    .weight(8f)
-            )
-            TextButton(
-                onClick = {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CustomTextField(prompt = prompt, onPromptChange = { prompt = it })
+            Spacer(modifier = Modifier.width(8.dp))
+            SendButton(
+                sendMessage = {
                     if (prompt.isNotBlank()) {
                         requestPlanTravel(prompt)
                     }
-                },
-
-                modifier = Modifier
-                    .weight(2f)
-                    .padding(all = 4.dp)
-                    .align(Alignment.CenterVertically)
-            ) {
-                Text(stringResource(R.string.action_go))
-            }
+                }
+            )
         }
         StateContainer(
             uiState = uiState,
@@ -97,4 +93,10 @@ fun ChatbotScreen(
             }
         }
     }
+}
+
+@Composable
+@Preview
+fun ChatbotScreenPreview() {
+    ChatbotScreen()
 }
