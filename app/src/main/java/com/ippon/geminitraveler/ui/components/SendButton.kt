227225489ec25bd32638bc80dc3693dd73285312
Justine Pataco <justinepataco@gmail.com>
2024-01-25 14:14:27 +0100

    :lipstick: [#2] Fix: Corrected icon button size

--- app/src/main/java/com/ippon/geminitraveler/ui/components/SendButton.kt
+++ app/src/main/java/com/ippon/geminitraveler/ui/components/SendButton.kt
@@ -1,6 +1,5 @@
 package com.ippon.geminitraveler.ui.components
 
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.FilledIconButton
@@ -8,19 +7,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.ippon.geminitraveler.ui.theme.md_theme_light_onPrimary
 import com.ippon.geminitraveler.ui.theme.md_theme_light_primary
 
 @Composable
-fun SendButton(sendMessage: () -> Unit) {
+fun SendButton(sendMessage: () -> Unit, modifier: Modifier) {
     FilledIconButton(
         onClick = { sendMessage() },
         colors = IconButtonDefaults.filledIconButtonColors(
             containerColor = md_theme_light_primary,
             contentColor = md_theme_light_onPrimary,
         ),
-        modifier = Modifier.size(48.dp)
+        modifier = modifier
     ) {
         Icon(imageVector = Icons.Filled.Send, contentDescription = "Send message")
     }
