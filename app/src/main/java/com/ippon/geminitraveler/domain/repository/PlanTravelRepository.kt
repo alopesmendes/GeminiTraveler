93f971419bf083d18a88d1926fdd885d457a3178
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-01-29 17:31:37 +0100

    [#20] :recycle: refactor PlanTravelRepository return type

--- app/src/main/java/com/ippon/geminitraveler/domain/repository/PlanTravelRepository.kt
+++ app/src/main/java/com/ippon/geminitraveler/domain/repository/PlanTravelRepository.kt
@@ -1,8 +1,10 @@
 package com.ippon.geminitraveler.domain.repository
 
+import com.ippon.geminitraveler.core.utils.Resource
 import com.ippon.geminitraveler.domain.model.PlanTravel
 import com.ippon.geminitraveler.domain.model.RequestPlan
+import kotlinx.coroutines.flow.Flow
 
 interface PlanTravelRepository {
-    suspend fun getPlanTravel(requestPlan: RequestPlan): PlanTravel
+    fun getPlanTravel(requestPlan: RequestPlan): Flow<Resource<PlanTravel>>
 }
