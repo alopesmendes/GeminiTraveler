8b18a61e6656c39690a42d61f1c48fe5d90fb04b
Justine Pataco <justinepataco@gmail.com>
2024-01-31 15:05:51 +0100

    :lipstick: [#23] Added button to create new chat conversation

--- app/src/main/java/com/ippon/geminitraveler/ui/screens/ContainerScreen.kt
+++ app/src/main/java/com/ippon/geminitraveler/ui/screens/ContainerScreen.kt
@@ -1,17 +1,19 @@
 package com.ippon.geminitraveler.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,12 +22,16 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ippon.geminitraveler.R
+import com.ippon.geminitraveler.ui.components.CustomTopBar
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,46 +48,46 @@ fun ContainerScreen(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                Text("Drawer title", modifier = Modifier.padding(16.dp))
-                Divider()
-                NavigationDrawerItem(
-                    label = { Text(text = "Drawer Item") },
-                    selected = false,
-                    onClick = {
-                        scope.launch {
-                            drawerState.close()
-                        }
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column {
+                        Text(
+                            stringResource(id = R.string.history_title),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        // Use DrawerItem here
                     }
-                )
+                    Button(
+                        modifier = Modifier.padding(16.dp),
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                        }) {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = ""
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = stringResource(id = R.string.btn_new_chat))
+                    }
+                }
             }
         }
     ) {
         Scaffold(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Text(
-                            stringResource(id = R.string.app_title),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = {
-                            scope.launch {
-                                drawerState.apply {
-                                    if (isClosed) open() else close()
-                                }
-                            }
-                        }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Open chat history"
-                            )
-                        }
-                    },
-                    scrollBehavior = scrollBehavior,
+                CustomTopBar(
+                    scope = scope,
+                    drawerState = drawerState,
+                    scrollBehavior = scrollBehavior
                 )
             },
         ) { innerPadding ->
@@ -89,5 +95,13 @@ fun ContainerScreen(
                 content()
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun ContainerScreenPreview() {
+    ContainerScreen {
+        Text(text = "Je suis un chatbot")
     }
 }
