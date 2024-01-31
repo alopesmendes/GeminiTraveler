a1296bc3b827b49481245426c8d586db4bb3936d
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-01-31 09:51:36 +0100

    [#22] dependency injection for database

--- app/src/main/java/com/ippon/geminitraveler/di/AppModule.kt
+++ app/src/main/java/com/ippon/geminitraveler/di/AppModule.kt
@@ -1,7 +1,10 @@
 package com.ippon.geminitraveler.di
 
+import android.content.Context
 import com.google.ai.client.generativeai.GenerativeModel
 import com.ippon.geminitraveler.BuildConfig
+import com.ippon.geminitraveler.data.datasource.database.GeminiLocalDatabase
+import com.ippon.geminitraveler.data.datasource.database.dao.MessageDao
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
@@ -15,4 +18,14 @@ class AppModule {
         modelName = "gemini-pro",
         apiKey = BuildConfig.apiKey
     )
+
+    @Single
+    fun provideGeminiLocalDatabase(context: Context): GeminiLocalDatabase {
+        return GeminiLocalDatabase.getInstance(context)
+    }
+
+    @Single
+    fun provideMessageDao(database: GeminiLocalDatabase): MessageDao {
+        return database.messageDao()
+    }
 }
