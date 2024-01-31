ea6a2f373056acc59f1a7eba1efd8454724263c9
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-01-31 14:03:30 +0100

    [#22] :recycle: add save messages in database

--- app/src/main/java/com/ippon/geminitraveler/data/repository/ModelRepositoryImpl.kt
+++ app/src/main/java/com/ippon/geminitraveler/data/repository/ModelRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.ippon.geminitraveler.data.repository
 import com.ippon.geminitraveler.core.utils.Resource
 import com.ippon.geminitraveler.data.mappers.mapToPlanTravel
 import com.ippon.geminitraveler.domain.datasources.GenerativeDataSource
+import com.ippon.geminitraveler.domain.datasources.MessageDatasource
 import com.ippon.geminitraveler.domain.model.ModelResponse
 import com.ippon.geminitraveler.domain.model.ModelRequest
 import com.ippon.geminitraveler.domain.model.Role
@@ -14,27 +15,32 @@ import org.koin.core.annotation.Single
 @Single
 class ModelRepositoryImpl(
     private val generativeDataSource: GenerativeDataSource,
+    private val messageDatasource: MessageDatasource,
 ): ModelRepository {
 
     override fun getPlanTravel(requestPlan: ModelRequest): Flow<Resource<ModelResponse>> = flow {
         try {
             // User Input
-            val userResource = Resource.Success(requestPlan.mapToPlanTravel())
+            val message = requestPlan.mapToPlanTravel()
+            val userResource = Resource.Success(message)
             emit(userResource)
+
+            messageDatasource.addMessage(message)
 
             // Waiting for AI model response
             emit(Resource.Loading)
 
             // AI model response
-            val modelResponse = generativeDataSource.generateContent(requestPlan.data)
-            emit(
-                Resource.Success(
-                    ModelResponse(
-                        data = modelResponse ?: "",
-                        role = Role.MODEL
-                    )
-                )
+            val promptMessage = generativeDataSource.generateContent(requestPlan.data)
+            val modelResponse = ModelResponse(
+                data = promptMessage ?: "",
+                role = Role.MODEL
             )
+            emit(
+                Resource.Success(modelResponse)
+            )
+
+            messageDatasource.addMessage(modelResponse)
         } catch (e: Exception) {
             emit(
                 Resource.Error(
