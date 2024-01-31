9275bccbec27037408bc193f30d0bef6ec4ba18a
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2024-01-31 12:38:02 +0100

    [#22] :recycle: add mapping to MessageEntity

--- app/src/main/java/com/ippon/geminitraveler/data/mappers/ModelMapper.kt
+++ app/src/main/java/com/ippon/geminitraveler/data/mappers/ModelMapper.kt
@@ -1,5 +1,6 @@
 package com.ippon.geminitraveler.data.mappers
 
+import com.ippon.geminitraveler.data.datasource.database.entities.MessageEntity
 import com.ippon.geminitraveler.domain.model.ModelResponse
 import com.ippon.geminitraveler.domain.model.ModelRequest
 import com.ippon.geminitraveler.domain.model.Role
@@ -7,4 +8,9 @@ import com.ippon.geminitraveler.domain.model.Role
 fun ModelRequest.mapToPlanTravel(): ModelResponse = ModelResponse(
     data = data,
     role = Role.USER,
+)
+
+fun ModelResponse.mapToMessageEntity(): MessageEntity = MessageEntity(
+    message = data,
+    isGemini = role == Role.MODEL,
 )
